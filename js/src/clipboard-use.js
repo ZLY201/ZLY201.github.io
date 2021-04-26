9c8c5365f9f6832cd2af8a94bb8b9f8040e567d2
yaozilong <951711127@qq.com>
2021-04-26 21:40:03 +0800

    Site updated: 2021-04-26 21:39:47

---
+++ js/src/clipboard-use.js
@@ -0,0 +1,17 @@
+/*ҳ��������ɺ󣬴������ư�ť*/
+!function (e, t, a) { 
+  /* code */
+  var initCopyCode = function(){
+    var copyHtml = '';
+    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
+    copyHtml += '  <i class="fa fa-globe"></i><span>Copy</span>';
+    copyHtml += '</button>';
+    $(".highlight .code pre").before(copyHtml);
+    new ClipboardJS('.btn-copy', {
+        target: function(trigger) {
+            return trigger.nextElementSibling;
+        }
+    });
+  }
+  initCopyCode();
+}(window, document);
